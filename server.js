11b3b68a1b1f9ad059172ff5064a52cb60685dc4
Matthew Donovan <matthewbryantdonovan@gmail.com>
2020-02-20 15:37:25 -0500

    solution array made

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-fs.readFile('f_libraries_of_the_world.txt', (err, data) => {
+fs.readFile('a_example.txt', (err, data) => {
     let dataDone = false;
     if (err) throw err;
     var allData = data.toString().split(`\n`);
@@ -29,7 +29,9 @@ fs.readFile('f_libraries_of_the_world.txt', (err, data) => {
                 books: currentLib[0],
                 signup: currentLib[1],
                 perDay: currentLib[2],
-                catalog: []
+                catalog: [],
+                pointsPerDay: [],
+                booksToSend: []
             }
             for (let index = 0; index < currentBooks.length; index++) {
                 allLibs[currentObj].catalog.push({
@@ -37,11 +39,68 @@ fs.readFile('f_libraries_of_the_world.txt', (err, data) => {
                     bookVal: scores[currentBooks[index]]
                 })
             }
+            allLibs.forEach(i => {
+                i.catalog.sort((a, b) => (a.bookVal < b.bookVal) ? 1 : -1);
+            })
+            for (let index = 0; index < totalDays; index++) {
+                let daysAfterSign = (totalDays-index)-allLibs[currentObj].signup;
+                let booksToSend = (daysAfterSign*allLibs[currentObj].perDay);
+                var totalVal = 0;
+                for (let index = 0; index < (booksToSend); index++) {
+                    
+                    if (index < (allLibs[currentObj].catalog.length)){
+                        let rando = allLibs[currentObj].catalog[index].bookVal;
+                        totalVal += parseInt(rando);
+                    }
+                }
+                
+                allLibs[currentObj].pointsPerDay.push({totalVal})
+                allLibs[currentObj].booksToSend.push({booksToSend})
+                
+            }
             currentObj++;
+
         }
     }
 
-    console.log(allLibs);
+    var solutionArr = [];
+    
+    for (var currentDay = 0; currentDay < totalDays; currentDay++) {
+        var currentTop = 0;
+        var currentTopPos = -1;
+        var changed = false;
+        for (var currentPos = 0; currentPos < allLibs.length; currentPos++) {
+            console.log("this");
+            
+            console.log(allLibs[currentPos].pointsPerDay[currentDay].totalVal);
+            console.log("vs");
+            
+            console.log(currentTop);
+            console.log("----");
+                        
+            
+            if (allLibs[currentPos].pointsPerDay[currentDay].totalVal > currentTop){
+                currentTop = allLibs[currentPos].pointsPerDay[currentDay].totalVal;
+                currentTopPos = currentPos;
+                changed = true;
+                console.log("I changed");
+                
+            }
+        }
+
+        if(changed == true){
+            currentDay += allLibs[currentTopPos].signup-1;
+            solutionArr.push(allLibs[currentTopPos]);
+            allLibs.splice(currentTopPos, 1)
+        }
+    }
+
+
+
+    console.log(JSON.stringify(allLibs));
+    console.log("----------------------------");
+    console.log("----------------------------");
+    console.log(JSON.stringify(solutionArr));
 
 
 });
