f4b280ac9af8ec8b41541aa230419842dd66e8b9
Matthew Donovan <matthewbryantdonovan@gmail.com>
2020-02-20 14:06:15 -0500

    test

--- server.js
+++ server.js
@@ -1,35 +1,47 @@
 const fs = require('fs');
 
-fs.readFile('a_example.txt', (err, data) => { 
-    
-    if (err) throw err; 
+fs.readFile('f_libraries_of_the_world.txt', (err, data) => {
+    let dataDone = false;
+    if (err) throw err;
     var allData = data.toString().split(`\n`);
-    allData.splice((allData.length-1), 1)
+    while(!dataDone){
+        if (allData[allData.length - 1] == ""){
+            allData.splice((allData.length - 1), 1)
+        } else {
+            dataDone = true;
+        }
+    }
+    
     var line1 = allData[0].split(" ");
     var scores = allData[1].split(" ");
     allData.splice((allData[0]), 2)
-    var books = parseInt(line1[0]);
-    var libs = parseInt(line1[1]);
-    var days = parseInt(line1[2]);
-    console.log(allData);
+    var totalBooks = parseInt(line1[0]);
+    var totalLibs = parseInt(line1[1]);
+    var totalDays = parseInt(line1[2]);
+    var currentObj = 0;
+    var allLibs = [];
     for (let index = 0; index < allData.length; index++) {
-        if ((index%2) == 0){
-            let tempLine = allData[index].split(" ");;
-            
-        } else {
-
+        if ((index % 2) == 0) {
+            let currentLib = allData[index].split(" ");
+            let currentBooks = allData[index + 1].split(" ");
+            allLibs[currentObj] = {
+                libPos: currentObj,
+                books: currentLib[0],
+                signup: currentLib[1],
+                perDay: currentLib[2],
+                catalog: []
+            }
+            for (let index = 0; index < currentBooks.length; index++) {
+                allLibs[currentObj].catalog.push({
+                    bookPos: currentBooks[index],
+                    bookVal: scores[currentBooks[index]]
+                })
+            }
+            currentObj++;
         }
     }
-    // allData.splice((allData.length-1), 1)
-    // var sliceMax = allData[0].split(" ");
-    // var sliceMax = parseInt(sliceMax[0]);
-    // var pizzaTypes = allData[0].split(" ");
-    // var pizzaTypes = parseInt(pizzaTypes[1]);
-    // var everyPizza = allData[1].split(" ");
-    // var answerPizzaTypes = [];
-    // var answerSliceTotal = 0;
-    // var tempArray = [];
-    // var fixedStuff = false;
-    // var answerTotalCheck = 0;
-    
-})
+
+    console.log(allLibs);
+
+
+});
