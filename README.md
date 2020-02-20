f9a7e1aaa92de8a0bc5817073ecbd762ec1ea824
Matthew Donovan <matthewbryantdonovan@gmail.com>
2020-02-20 13:30:01 -0500

    grabbing numbers

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # hashCode
+6 books 2 lib 7days
+1 2 3 6 5 4 -- points for each book 
+
+lib 0
+-------
+5 books 2 signup days 2 books per day
+0 1 2 3 4 what books can be sent 0 = 1 pt, 1 = 2 pts, etc.
+------
+4 3 1
+0 2 3 5
+
--- server.js
+++ server.js
@@ -1,1 +1,36 @@
 const fs = require('fs');
+
+fs.readFile('a_example.txt', (err, data) => { 
+    
+    if (err) throw err; 
+    var allData = data.toString().split(`\n`);
+    allData.splice((allData.length-1), 1)
+    console.log(allData);
+    var line1 = allData[0].split(" ");
+    var scores = allData[1].split(" ");
+    allData.splice((allData[0]), 2)
+    var books = parseInt(line1[0]);
+    var libs = parseInt(line1[1]);
+    var days = parseInt(line1[2]);
+    console.log(allData);
+    for (let index = 0; index < allData.length; index++) {
+
+        
+    }
+    console.log(books);
+    console.log(libs);
+    console.log(days);
+    console.log(scores);
+    // allData.splice((allData.length-1), 1)
+    // var sliceMax = allData[0].split(" ");
+    // var sliceMax = parseInt(sliceMax[0]);
+    // var pizzaTypes = allData[0].split(" ");
+    // var pizzaTypes = parseInt(pizzaTypes[1]);
+    // var everyPizza = allData[1].split(" ");
+    // var answerPizzaTypes = [];
+    // var answerSliceTotal = 0;
+    // var tempArray = [];
+    // var fixedStuff = false;
+    // var answerTotalCheck = 0;
+    
+})
